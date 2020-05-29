45dc6963cda8bcc62ac60758beb31a3c4810e6cf
ALWAYSROHITKANSARA <rohitkansara75@gmail.com>
2020-05-29 17:58:35 +0530

    final update

--- src/game.h
+++ src/game.h
@@ -10,14 +10,26 @@
 class Game {
  public:
   Game(std::size_t grid_width, std::size_t grid_height);
-  void Run(Controller const &controller, Renderer &renderer,
+  void Run(Controller const &controller, Renderer *renderer,
            std::size_t target_frame_duration);
   int GetScore() const;
   int GetSize() const;
+  // methods for pausing/resuming the game
+  void TriggerPause();
+
+ friend class Controller;
+
+ protected:
+  bool _wall; // wall around the game screen
 
  private:
   Snake snake;
   SDL_Point food;
+  // Renderer _renderer;
+  // pause state
+  // true if game is paused
+  bool _paused;
+  bool _poisoned;
 
   std::random_device dev;
   std::mt19937 engine;
@@ -27,7 +39,10 @@ class Game {
   int score{0};
 
   void PlaceFood();
-  void Update();
+  void Update(Renderer *renderer);
+  void Pause();
+  void Resume();
+
 };
 
 #endif
